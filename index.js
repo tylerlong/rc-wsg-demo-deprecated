f2aa3c007fb2c8c55c98ea0de02f0270a3579cfd
Tyler Long <tyler4long@gmail.com>
2018-07-10 12:02:44 +0800

    Refactor code

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 ;(async () => {
-  const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER_URL)
-
-  try {
-    await rc.authorize({
-      username: process.env.RINGCENTRAL_USERNAME,
-      extension: process.env.RINGCENTRAL_EXTENSION,
-      password: process.env.RINGCENTRAL_PASSWORD
-    })
-    console.log('access_token:', rc.token().access_token)
-  } catch (e) {
-    console.log(e.response.data)
-  }
+  const rc = new RingCentral(
+    process.env.RINGCENTRAL_CLIENT_ID,
+    process.env.RINGCENTRAL_CLIENT_SECRET,
+    process.env.RINGCENTRAL_SERVER_URL
+  )
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD
+  })
+  const accessToken = rc.token().access_token
 
   const ws = new WS(process.env.RINGCENTRAL_WSG_URL)
-
   ws.on('open', function open () {
     console.log('open')
     ws.send([
@@ -29,12 +27,11 @@ dotenv.config()
         'method': 'GET',
         'path': '/restapi/v1.0/account/~/extension/~',
         'headers': {
-          'Authorization': `Bearer ${rc.token().access_token}`
+          'Authorization': `Bearer ${accessToken}`
         }
       }
     ])
   })
-
   ws.on('message', function incoming (data) {
     console.log('message', data)
   })
